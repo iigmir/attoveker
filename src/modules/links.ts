1e476bfb44300549a8fc8af3df03132e2e027762
iigmir <roc120j@gmail.com>
2024-04-30 10:50:46 +0800

    Add functions

--- src/modules/links.ts
+++ src/modules/links.ts
@@ -5,40 +5,44 @@ export interface BasicLinkInterface {
 }
 
 /**
- * For example, `<a href="https://attackers.net/actress/detail/351897">松下紗栄子</a>` will be:
- * ```json
- * { name: "松下紗栄子", id: "351897", link: "https://attackers.net/actress/detail/351897" }
- * ```
+ * ========== GetDataType MODULES ==========
  */
-export interface ActorInterface extends BasicLinkInterface {}
 
 enum GetDataType {
     Actor = "actor",
     Genre = "genre",
+    Label = "label",
+    Series = "series",
 }
+const get_selector = (type: GetDataType) => {
+    switch (type) {
+        case GetDataType.Actor: return `a[href*="actress/detail"]`;
+        case GetDataType.Genre: return `a[href*="works/list/genre"]`;
+        case GetDataType.Label: return `a[href*="works/list/label"]`;
+        case GetDataType.Series: return `a[href*="works/list/series"]`;
+        default: return "";
+    }
+};
+const get_detection_regex = (type: GetDataType) => {
+    switch (type) {
+        case GetDataType.Actor: return /\/actress\/detail\/([0-9]+)(\?|)/g;
+        case GetDataType.Genre: return /\/works\/list\/genre\/([0-9]+)(\?|)/g;
+        case GetDataType.Label: return /\/works\/list\/label\/([0-9]+)(\?|)/g;
+        case GetDataType.Series: return /\/works\/list\/series\/([0-9]+)(\?|)/g;
+        default: return /\w/g;
+    }
+};
+const get_replacing_regex = (type: GetDataType) => {
+    switch (type) {
+        case GetDataType.Actor: return /\/actress\/detail\//g;
+        case GetDataType.Genre: return /\/works\/list\/genre\//g;
+        case GetDataType.Label: return /\/works\/list\/label\//g;
+        case GetDataType.Series: return /\/works\/list\/series\//g;
+        default: return /\w/g;
+    }
+};
 
 export function GetData(a: Element | HTMLAnchorElement, type: GetDataType): BasicLinkInterface {
-    const get_selector = (type: GetDataType) => {
-        switch (type) {
-            case GetDataType.Actor: return `a[href*="actress/detail"]`;
-            case GetDataType.Genre: return `a[href*="works/list/genre"]`;
-            default: return "";
-        }
-    };
-    const get_detection_regex = (type: GetDataType) => {
-        switch (type) {
-            case GetDataType.Actor: return /\/actress\/detail\/([0-9]+)(\?|)/g;
-            case GetDataType.Genre: return /\/works\/list\/genre\/([0-9]+)(\?|)/g;
-            default: return /\w/g;
-        }
-    };
-    const get_replacing_regex = (type: GetDataType) => {
-        switch (type) {
-            case GetDataType.Actor: return /\/actress\/detail\//g;
-            case GetDataType.Genre: return /\/works\/list\/genre\//g;
-            default: return /\w/g;
-        }
-    };
     const get_dom = (a: Element | HTMLAnchorElement): HTMLAnchorElement | null => {
         if( a.hasAttribute("href") ) {
             return a as HTMLAnchorElement;
@@ -77,6 +81,15 @@ export function GetData(a: Element | HTMLAnchorElement, type: GetDataType): Basi
 }
 
 /**
+ * ========== ACTOR MODULES ==========
+ */
+
+/**
+ * @example { name: "松下紗栄子", id: "351897", link: "https://attackers.net/actress/detail/351897" }
+ */
+export interface ActorInterface extends BasicLinkInterface {}
+
+/**
  * getActorData('<a href="https://attackers.net/actress/detail/351897">松下紗栄子</a>')
  * // returns { name: "松下紗栄子", id: "351897", link: "https://attackers.net/actress/detail/351897" }
  */
@@ -84,18 +97,45 @@ export function getActorData(a: Element | HTMLAnchorElement): ActorInterface {
     return GetData(a, GetDataType.Actor);
 }
 
-export function getGenreData(a: Element | HTMLAnchorElement): BasicLinkInterface {
+export function getActorDatas(ary: Element[]): ActorInterface[] {
+    return ary.map( getActorData );
+}
+
+/**
+ * ========== GENRE MODULES ==========
+ */
+
+export interface GenreInterface extends BasicLinkInterface {}
+
+export function getGenreData(a: Element | HTMLAnchorElement): GenreInterface {
     return GetData(a, GetDataType.Genre);
 }
-export function getGenreDatas(ary: Element[]): BasicLinkInterface[] {
+export function getGenreDatas(ary: Element[]): GenreInterface[] {
     return ary.map( getGenreData );
 }
 
 /**
- * Get all actors!
- * @param ary Many DOMs!
- * @returns Many actors!
+ * ========== LABEL MODULES ==========
  */
-export function getActorDatas(ary: Element[]): ActorInterface[] {
-    return ary.map( getActorData );
+
+export interface LabelInterface extends BasicLinkInterface {}
+
+export function getLabelData(a: Element | HTMLAnchorElement): LabelInterface {
+    return GetData(a, GetDataType.Label);
+}
+export function getLabelDatas(ary: Element[]): LabelInterface[] {
+    return ary.map( getLabelData );
+}
+
+/**
+ * ========== SERIES MODULES ==========
+ */
+
+export interface SeriesInterface extends BasicLinkInterface {}
+
+export function getSeriesData(a: Element | HTMLAnchorElement): SeriesInterface {
+    return GetData(a, GetDataType.Series);
+}
+export function getSeriesDatas(ary: Element[]): SeriesInterface[] {
+    return ary.map( getSeriesData );
 }
