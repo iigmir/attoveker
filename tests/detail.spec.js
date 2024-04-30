86c8d4d8ea53d091e08a8baeba5e8843257d9ceb
iigmir <roc120j@gmail.com>
2024-04-30 11:01:27 +0800

    Add 404 case

--- tests/detail.spec.js
+++ tests/detail.spec.js
@@ -3,6 +3,25 @@ import supertest from "supertest";
 import app from "../dist/index.js";
 
 describe("GET /detail", () => {
+    describe( "/NOTHING404", () => {
+        let api = {};
+        before((done) => {
+            const ajax = supertest(app).get("/detail/NOTHING404");
+            ajax.expect(404).end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                api = res.body;
+                done();
+            });
+        });
+        it("shows error", () => {
+            strictEqual(api.message, "video not found");
+        });
+        it("can get nothing", () => {
+            strictEqual(api.result.title, "");
+        });
+    });
     describe( "/ADN551", () => {
         let api = {};
         before((done) => {
@@ -15,16 +34,18 @@ describe("GET /detail", () => {
                 done();
             });
         });
-        it("can get videos infomation", () => {
+        it("can get video's metadata", () => {
             strictEqual(api.title, "人妻寝取られ仮面パーティー");
             strictEqual(api.description, "大口の取引先が倒産。資金繰りに行き詰った僕は、藁にも縋る思いで梅田さんに助けを求めた。すると彼は「起業家が集まるパーティーに行ってみないか？」と提案。なんだか怪しいとは思ったが、そんな事を言ってる場合ではない。彼に連れられて会場に入ると、そこは異様な雰囲気に包まれていた。参加者全員が仮面を付け、目の前で行われているセックス愉しんで観ている。次は誰がセックスするんだろう…そう思っていたら、妻によく似た女性が観衆の前でセックスをはじめた。");
             strictEqual(api.director, "きとるね川口");
-            strictEqual(api.actors.length, 1);
-            strictEqual(api.genre.length, 4);
+            strictEqual(api.preview, "https://cc3001.dmm.co.jp/litevideo/freepv/a/adn/adn00551/adn00551_dm_w.mp4");
             strictEqual(api.id, "ADN551");
             strictEqual(api.time, "160");
             strictEqual(api.price, "4598");
-            strictEqual(api.preview, "https://cc3001.dmm.co.jp/litevideo/freepv/a/adn/adn00551/adn00551_dm_w.mp4");
+        });
+        it("can get video's list datas", () => {
+            strictEqual(api.actors.length, 1);
+            strictEqual(api.genre.length, 4);
         });
     });
     describe( "/ATID141", () => {
@@ -39,16 +60,18 @@ describe("GET /detail", () => {
                 done();
             });
         });
-        it("can get videos infomation", () => {
+        it("can get video's metadata", () => {
             strictEqual(api.title, "アナル奴隷白書");
             strictEqual(api.description, "さや(真琴)は父と2人で幸せに暮らしていた。そんな日常に暗雲が立ち籠め始める…。【6千万円】騙されて多額の借金を背負ってしまった父と娘。逃げ出す事もできずに娘は男達の巨大な肉棒を捩じ込まれて…。");
             strictEqual(api.director, "芳賀栄太郎");
-            strictEqual(api.actors.length, 1);
-            strictEqual(api.genre.length, 1);
             strictEqual(api.id, "ATID141");
             strictEqual(api.time, "110");
             strictEqual(api.price, "2940");
             strictEqual(api.preview, "https://cc3001.dmm.co.jp/litevideo/freepv/a/ati/atid141/atid141_dmb_s.mp4");
+        });
+        it("can get video's list datas", () => {
+            strictEqual(api.actors.length, 1);
+            strictEqual(api.genre.length, 1);
         });
     });
     describe( "/ATKD370", () => {
@@ -63,16 +86,18 @@ describe("GET /detail", () => {
                 done();
             });
         });
-        it("can get videos infomation", () => {
+        it("can get video's metadata", () => {
             strictEqual(api.title, "中年オヤジの唾液まみれのベロキス性交に堕ちた美女8時間");
             strictEqual(api.description, "美女の脳を溶かす、オヤジたちのベロキス性交。美女は犯されながらキスする顔が一番エロい。接吻美女20名収録。");
             strictEqual(api.director, "");
-            strictEqual(api.actors.length, 20);
-            strictEqual(api.genre.length, 3);
             strictEqual(api.id, "ATKD370");
             strictEqual(api.time, "480");
             strictEqual(api.price, "3498");
             strictEqual(api.preview, "https://cc3001.dmm.co.jp/litevideo/freepv/a/atk/atkd00370/atkd00370_dm_w.mp4");
+        });
+        it("can get video's list datas", () => {
+            strictEqual(api.actors.length, 20);
+            strictEqual(api.genre.length, 3);
         });
     });
 });
